47e38d1ff10279c05fe5d11686de5007bd147c3b
Nicolas Demonte <nicolas@affinitic.be>
2017-09-12 11:45:21 +0200

    Initial commit

---
+++ README.rst
@@ -0,0 +1,4 @@
+Affinitic logging
+===================
+
+Adds custom information to Plone text error logs by monkey patching Products.SiteErrorLog
---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1.dev0'
+
+setup(name='affinitic.logging',
+      version=version,
+      description="Adds information to text logs",
+      long_description=open("README.rst").read() + "\n" +
+      open("CHANGES.rst").read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.4",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "Framework :: Plone",
+          "Framework :: Plone :: 3.3",
+      ],
+      keywords='',
+      author='Nicolas Demonte',
+      author_email='support@lists.affinitic.be',
+      url='https://github.com/affinitic/affinitic.logging',
+      license='GPL',
+      packages=find_packages('src', exclude=['ez_setup']),
+      package_dir={'': 'src'},
+      namespace_packages=['affinitic'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+      ],
+      extras_require=dict(
+          test=['unittest2'],
+      ),
+      entry_points={
+'console_scripts': []})
